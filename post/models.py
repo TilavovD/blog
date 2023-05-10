bba067cced099d64d7a4d7454d1486af9f021d97
TilavovD <tilavov.dostonbek@gmail.com>
2023-05-10 12:06:50 +0500

    added login view and template

--- post/models.py
+++ post/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.title
+
 
 class Post(models.Model):
-	title = models.CharField(max_length=32)
-	slug = models.CharField(max_length=64)
-	body = models.TextField(max_length=64)
-	
-	def __str__(self):
-		return self.title
+    title = models.CharField(max_length=32)
+    slug = models.CharField(max_length=64)
+    body = models.TextField(max_length=64)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    created_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
